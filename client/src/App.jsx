706965ece43aff7642a9726236300d4ddf4c208c
238w1a1283 <chouthaakshay02@gmail.com>
2026-04-17 10:00:41 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+import Addproduct from './components/Addproduct'
+import Home from './components/Home'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path ="/add-product" element={
+          <ProtectedRoute>
+            <Addproduct/>
+          </ProtectedRoute>
+        }></Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
